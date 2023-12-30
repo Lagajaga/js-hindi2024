d0661883896b80d76e1afe229f0a02f5c77e3ed7
Lagajaga <131897128+Lagajaga@users.noreply.github.com>
2023-12-30 14:52:19 +0000

    ok

---
+++ .devcontainer/01_basics/datatypes.js
@@ -0,0 +1,11 @@
+// number=2 to power 53
+// boolean=true or false
+// bigint =integar for big value
+//null = act as a object
+//undefined = undefined 
+let varr;
+// let jagadish="jaga@gmail.com"// string data
+// symbol = for uniqueness
+ const juggy="blademaster"
+
+console.log(typeof juggy )
